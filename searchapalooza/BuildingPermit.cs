c38e4c9d0a2f704f987e413b69043ffe7655a7ad
Simon Timms <stimms@gmail.com>
2020-01-04 23:07:46 -0700

    Initial add

---
+++ searchapalooza/BuildingPermit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace searchapalooza
+{
+    public class BuildingPermit
+    {
+
+        public string PermitNum { get; set; }
+        public string StatusCurrent { get; set; }
+        public DateTime? AppliedDate { get; set; }
+
+        public DateTime? IssuedDate { get; set; }
+
+        public DateTime? CompletedDate { get; set; }
+
+        public string PermitType { get; set; }
+        public string PermitTypeMapped { get; set; }
+        public string PermitClass { get; set; }
+        public string PermitClassGroup { get; set; }
+        public string PermitClassMapped { get; set; }
+        public string WorkClass { get; set; }
+        public string WorkClassGroup { get; set; }
+        public string WorkClassMapped { get; set; }
+        public string Description { get; set; }
+        public string ContractorName { get; set; }
+        public string HousingUnits { get; set; }
+        public int? EstProjectCost { get; set; }
+        public int? TotalSqFt { get; set; }
+        public string OriginalAddress { get; set; }
+        public string CommunityCode { get; set; }
+        public string CommunityName { get; set; }
+        public decimal? Latitude { get; set; }
+        public decimal? Longitude { get; set; }
+        public string Location { get; set; }
+        public int? LocationCount { get; set; }
+        public string LocationTypes { get; set; }
+        public string LocationAddresses { get; set; }
+        public string LocationsWKT { get; set; }
+        public string LocationsGeoJSON { get; set; }
+    }
+}
---
+++ searchapalooza/Program.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace searchapalooza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("Building_Permits.csv"))
+            using (var csv = new CsvReader(reader))
+            {
+                var records = csv.GetRecords<BuildingPermit>().ToList();
+              
+            }
+        }
+    }
+}
